94a4932c137d193aa7852ace27c2ef2e0900d70d
rumiantsevaa <89034072+rumiantsevaa@users.noreply.github.com>
2025-01-03 23:22:59 +0200

    Delete netdata/save_dashboard.sh

--- netdata/save_dashboard.sh
+++
@@ -1,9 +0,0 @@
-# /netdata/save_dashboard.sh
-#!/bin/bash
-OUTPUT_DIR="/var/log/netdata/dashboards"
-GITHUB_DIR="/project/netdata/dashboards"
-mkdir -p $OUTPUT_DIR
-mkdir -p $GITHUB_DIR
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-curl -o "${OUTPUT_DIR}/netdata_${TIMESTAMP}.html" http://127.0.0.1:19999
-cp "${OUTPUT_DIR}/netdata_${TIMESTAMP}.html" "${GITHUB_DIR}/latest_dashboard.html"
