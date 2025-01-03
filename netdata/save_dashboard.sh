ecc83e04c6cfa6a38deb5c659ab493db9f48f726
rumiantsevaa <89034072+rumiantsevaa@users.noreply.github.com>
2025-01-03 22:21:06 +0200

    Update save_dashboard.sh

--- netdata/save_dashboard.sh
+++ netdata/save_dashboard.sh
@@ -1,7 +1,7 @@
 # /netdata/save_dashboard.sh
 #!/bin/bash
 OUTPUT_DIR="/var/log/netdata/dashboards"
-GITHUB_DIR="/path/to/project/netdata/dashboards"
+GITHUB_DIR="/project/netdata/dashboards"
 mkdir -p $OUTPUT_DIR
 mkdir -p $GITHUB_DIR
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
