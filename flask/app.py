ca1b4540a2ac47e1eefd9d6095d1e6da16e07b7c
rumiantsevaa <89034072+rumiantsevaa@users.noreply.github.com>
2025-01-18 09:56:12 +0200

    "trusted-types 'none' default; " -> "trusted-types default; "
    csp = "trusted-types default; "

--- flask/app.py
+++ flask/app.py
@@ -91,7 +91,7 @@ def add_security_headers(response):
         "base-uri 'self'; "
         "form-action 'self'; "
         "require-trusted-types-for 'script'; "
-	"trusted-types 'none' default; "
+	"trusted-types default; "
         "object-src 'none'"
     ).format(nonce=nonce)
     response.headers['Content-Security-Policy'] = csp
